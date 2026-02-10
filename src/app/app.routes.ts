14dcc4b415d27b2c2ace0fc85642559575662d7b
mohammedennaim <mohammedennaim99@gmail.com>
2026-02-10 09:08:03 +0100

    Ajouter les variables d'environnement pour l'API Adzuna et mettre à jour les routes pour utiliser le chargement paresseux des composants.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'login',
-        loadChildren: () => import('./features/auth/login-modal/login-modal.component').then(m => m.LoginModalComponent)
+        loadComponent: () => import('./features/auth/login-modal/login-modal.component').then(m => m.LoginModalComponent)
     },
     {
         path: 'register',
-        loadChildren: () => import('./features/auth/register-modal/register-modal.component').then(m => m.RegisterModalComponent)
+        loadComponent: () => import('./features/auth/register-modal/register-modal.component').then(m => m.RegisterModalComponent)
     },
     {
         path: '**',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     jsonServerUrl: 'http://localhost:3000',
     adzuna: {
-        appId: 'c2f3cdb8',
-        appKey: 'e27c409e7f9a35ccd7a77c51e0eacf9a',
-        apiUrl: 'https://api.adzuna.com/v1/api'
+        appId: import.meta.env['NG_APP_APP_ID'],
+        appKey: import.meta.env['NG_APP_APP_KEY'],
+        apiUrl: import.meta.env['NG_APP_API_URL']
     }
 };
